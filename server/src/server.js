0d5870fed2e192e02b20274620f7e2f91ca0521c
kmalski <Konrad.Malski@fis.agh.edu.pl>
2020-10-16 22:52:35 +0200

    Fix handling 404 on server side

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,12 @@ const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
+const path = require('path');
 
 const port = process.env.SERVER_PORT || 2222;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cors());
 
 const rooms = require('./socket/rooms');
@@ -22,7 +22,7 @@ io.on('connect', socket => {
 });
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
 server.listen(port, () => {
